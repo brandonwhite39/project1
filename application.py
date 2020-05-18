dd74654b277b243ff5b69bc77bb38311974fc48c
Brandon White <14blw@queensu.ca>
2020-05-18 17:22:19 -0400

    Inital commit, basic logging in functionality

---
+++ application.py
@@ -0,0 +1,46 @@
+import os
+
+from flask import Flask, session, render_template
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+app = Flask(__name__)
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+
+@app.route("/")
+def index():
+    return render_template("homepage.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/created", methods=["POST"])
+def created():
+    return render_template("success.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/home", methods=["POST"])
+def home():
+    return render_template("home.html")
+
+
+
