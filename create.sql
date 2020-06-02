79d9ec59dfa8b709a0da585083037e1f88b32246
Brandon White <14blw@queensu.ca>
2020-06-02 18:04:21 -0400

    Added foreign key refernce to review table, put code in PostgreSQL

--- create.sql
+++ create.sql
@@ -1,12 +1,14 @@
 CREATE TABLE books(
-    isbn VARCHAR PRIMARY KEY,
+    isbn VARCHAR,
     title VARCHAR NOT NULL,
     author VARCHAR NOT NULL,
-    year INTEGER NOT NULL
+    year INTEGER NOT NULL,
+    PRIMARY KEY (isbn)
 );
 
 CREATE TABLE review(
-    isbn INTEGER PRIMARY KEY,
+    isbn VARCHAR REFERENCES books(isbn),
     rating FLOAT,
-    comments VARCHAR
+    comments VARCHAR,
+    PRIMARY KEY (isbn)
 );
